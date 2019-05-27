b9a2204456d7680859d15b186ae8871f3c5d8df8
feibai <wushi88@vip.qq.com>
2019-05-27 15:13:37 +0800

    thymeleaf模板学习

---
+++ src/main/java/com/feibai/springboot_demo/entity/Book.java
@@ -0,0 +1,18 @@
+package com.feibai.springboot_demo.entity;
+
+import lombok.Data;
+
+/**
+ * @Author: ${user}
+ * @description:
+ * @Date: Created in 14:52 2019/5/27
+ * @Modfied by: ${user}
+ * @Modfied Date by: 14:52 2019/5/27
+ */
+@Data
+public class Book {
+    private Integer id;
+    private String name;
+    private String author;
+
+}
